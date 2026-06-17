78a83f02668ac88d365e5cfe25833ae6e3fe3f5f
mikyas1299-glitch <mikyas1299@gmail.com>
2026-06-17 22:25:35 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const text = "Full Stack Developer";
+
+let i = 0;
+
+function typing(){
+
+if(i < text.length){
+
+document.getElementById("typing").innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(typing,100);
+
+}
+
+}
+
+typing();
